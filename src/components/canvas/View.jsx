499a8feea31ca8d6c1f31ab51241c41f827e5a93
mkubdev <mkubdev@gmail.com>
2023-10-28 11:08:23 +0200

    feat: add fourviere first blender export, UVs works

--- src/components/canvas/View.jsx
+++ src/components/canvas/View.jsx
@@ -1,9 +1,10 @@
 'use client'
 
-import { forwardRef, Suspense, useImperativeHandle, useRef } from 'react'
-import { OrbitControls, PerspectiveCamera, View as ViewImpl } from '@react-three/drei'
 import { Three } from '@/helpers/components/Three'
+import { Environment, OrbitControls, PerspectiveCamera, View as ViewImpl } from '@react-three/drei'
+import { Suspense, forwardRef, useImperativeHandle, useRef } from 'react'
 
+// Common is the ambient light and camera
 export const Common = ({ color }) => (
   <Suspense fallback={null}>
     {color && <color attach='background' args={[color]} />}
@@ -11,9 +12,20 @@ export const Common = ({ color }) => (
     <pointLight position={[20, 30, 10]} intensity={1} />
     <pointLight position={[-10, -10, -10]} color='blue' />
     <PerspectiveCamera makeDefault fov={40} position={[0, 0, 6]} />
+    <Environment preset="city" />
   </Suspense>
 )
 
+// SceneAndCamera is the ambient light and camera
+export const SceneAndCamera = ({ color }) => (
+	<Suspense fallback={null}>
+    {color && <color attach='background' args={[color]} />}
+		<Environment preset='city' />
+    <ambientLight intensity={0.3} />
+  </Suspense>
+)
+
+// View is the canvas
 const View = forwardRef(({ children, orbit, ...props }, ref) => {
   const localRef = useRef(null)
   useImperativeHandle(ref, () => localRef.current)
--- src/components/dom/Layout.jsx
+++ src/components/dom/Layout.jsx
@@ -1,38 +1,46 @@
 'use client'
 
-import { useRef } from 'react'
 import dynamic from 'next/dynamic'
+import { useRef } from 'react'
 const Scene = dynamic(() => import('@/components/canvas/Scene'), { ssr: false })
 
 const Layout = ({ children }) => {
-  const ref = useRef()
+	const ref = useRef()
 
-  return (
-    <div
-      ref={ref}
-      style={{
-        position: 'relative',
-        width: ' 100%',
-        height: '100%',
-        overflow: 'auto',
-        touchAction: 'auto',
-      }}
-    >
-      {children}
-      <Scene
-        style={{
-          position: 'fixed',
-          top: 0,
-          left: 0,
-          width: '100vw',
-          height: '100vh',
-          pointerEvents: 'none',
-        }}
-        eventSource={ref}
-        eventPrefix='client'
-      />
-    </div>
-  )
+	return (
+		<div
+			ref={ref}
+			style={{
+				position: 'relative',
+				width: ' 100%',
+				height: '100%',
+				overflow: 'auto',
+				touchAction: 'auto',
+			}}
+		>
+			{children}
+
+			{/* Scene is <Canvas /> */}
+			<Scene
+				style={{
+					position: 'fixed',
+					top: 0,
+					left: 0,
+					width: '100vw',
+					height: '100vh',
+					pointerEvents: 'none',
+				}}
+				eventSource={ref}
+				eventPrefix='client'
+
+				//? Where to put correctly the camera?
+				camera={{
+					// position: [5, 2, 5]
+				}}
+
+			/>
+		</div>
+	)
 }
 
 export { Layout }
